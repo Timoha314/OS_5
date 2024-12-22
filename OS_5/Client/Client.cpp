7b032c7401f73f41a70768468ed0a4ae43c02891
Basalyga Tsimafei <107477734+Timoha314@users.noreply.github.com>
2024-12-22 14:54:40 +0300

    Add files via upload

---
+++ OS_5/Client/Client.cpp
@@ -0,0 +1,49 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    HANDLE ReadPipe1 = GetStdHandle(STD_INPUT_HANDLE);
+    HANDLE WritePipe2 = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    DWORD bytesRead, bytesWritten;
+
+    long size, N, M;
+
+    // Чтение размера массива, N и M от Server
+    ReadFile(ReadPipe1, &size, sizeof(size), &bytesRead, NULL);
+    ReadFile(ReadPipe1, &N, sizeof(N), &bytesRead, NULL);
+    ReadFile(ReadPipe1, &M, sizeof(M), &bytesRead, NULL);
+
+    cout << "Received from Server:" << endl;
+    cout << "Array size: " << size << endl;
+    cout << "N: " << N << ", M: " << M << endl;
+
+    // Генерация элементов массива в диапазоне [N, M]
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<long> dis(N, M);
+
+    vector<long> generatedArray(size);
+    for (long& val : generatedArray) {
+        val = dis(gen);
+    }
+
+    cout << "Generated array: ";
+    for (long val : generatedArray) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Отправка массива обратно в Server
+    WriteFile(WritePipe2, generatedArray.data(), size * sizeof(long), &bytesWritten, NULL);
+
+    cout << "Press 'Q' to exit..." << endl;
+    while (1) {}
+
+    return 0;
+}
---
+++ OS_5/OS_5/Server.cpp
@@ -0,0 +1,100 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    HANDLE ReadPipe1, WritePipe1, ReadPipe2, WritePipe2;
+    SECURITY_ATTRIBUTES saAttr;
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.lpSecurityDescriptor = NULL;
+    saAttr.bInheritHandle = TRUE;
+
+    // Создание анонимных каналов
+    if (!CreatePipe(&ReadPipe1, &WritePipe1, &saAttr, 0)) {
+        cerr << "Error creating channel 1" << endl;
+        return 1;
+    }
+
+    if (!CreatePipe(&ReadPipe2, &WritePipe2, &saAttr, 0)) {
+        cerr << "Error creating channel 2" << endl;
+        return 1;
+    }
+
+    // Настройка дочернего процесса
+    STARTUPINFO si;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    si.hStdInput = ReadPipe1;
+    si.hStdOutput = WritePipe2;
+    si.dwFlags |= STARTF_USESTDHANDLES;
+
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&pi, sizeof(pi));
+
+    // Ввод данных
+    long n;
+    cout << "Enter array size (long): ";
+    cin >> n;
+
+    long N, M;
+    cout << "Enter N and M (N < M): ";
+    cin >> N >> M;
+
+    // Запуск процесса Client
+    wstring Arg = L"Client.exe";
+    if (!CreateProcess(NULL, &Arg[0], NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+        cerr << "Error starting Client process" << endl;
+        return 1;
+    }
+
+    // Передача данных Client'у
+    DWORD bytesWritten;
+    WriteFile(WritePipe1, &n, sizeof(n), &bytesWritten, NULL);
+    WriteFile(WritePipe1, &N, sizeof(N), &bytesWritten, NULL);
+    WriteFile(WritePipe1, &M, sizeof(M), &bytesWritten, NULL);
+
+    // Получение данных от Client'а
+    vector<long> receivedArray;
+    long temp;
+    DWORD bytesRead;
+
+    cout << "Waiting for data from Client..." << endl;
+    for (long i = 0; i < n; ++i) {
+        if (ReadFile(ReadPipe2, &temp, sizeof(temp), &bytesRead, NULL)) {
+            receivedArray.push_back(temp);
+        }
+        else {
+            cerr << "Error reading data from Client" << endl;
+            break;
+        }
+    }
+
+    // Вывод полученной информации
+    cout << "Received data from Client:" << endl;
+    for (long value : receivedArray) {
+        cout << value << " ";
+    }
+    cout << endl;
+
+    // Ожидание нажатия клавиши "Q"
+    cout << "Press 'Q' to exit." << endl;
+    while (true) {
+        if (_kbhit() && _getch() == 'Q') {
+            break;
+        }
+    }
+
+    // Завершение работы
+    CloseHandle(ReadPipe1);
+    CloseHandle(WritePipe1);
+    CloseHandle(ReadPipe2);
+    CloseHandle(WritePipe2);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    cout << "Program terminated." << endl;
+    return 0;
+}
